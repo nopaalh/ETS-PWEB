35a1a1d1eaee5fe19c7286a25d8aefeb1b570a44
nopalh <nopalh25@gmail.com>
2025-10-23 17:31:39 +0700

    Add 7 more Indonesian mountains to GunungSeeder (Merapi, Merbabu, Lawu, Arjuno-Welirang, Kelud, Ijen, Prau, Gede, Ciremai)

--- database/seeders/GunungSeeder.php
+++ database/seeders/GunungSeeder.php
@@ -54,5 +54,122 @@ class GunungSeeder extends Seeder
             'kuota_harian' => 30,
             'status' => 'active'
         ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Merapi',
+            'lokasi_provinsi' => 'Daerah Istimewa Yogyakarta',
+            'lokasi_kabupaten' => 'Kabupaten Sleman',
+            'ketinggian' => 2914,
+            'level_kesulitan' => 'medium',
+            'gambar' => 'merapi.jpg',
+            'deskripsi' => 'Gunung Merapi adalah gunung berapi aktif yang terletak di perbatasan Jawa Tengah dan Daerah Istimewa Yogyakarta. Dikenal sebagai gunung paling aktif di Indonesia.',
+            'harga_tiket' => 300000,
+            'kuota_harian' => 80,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Merbabu',
+            'lokasi_provinsi' => 'Jawa Tengah',
+            'lokasi_kabupaten' => 'Kabupaten Magelang',
+            'ketinggian' => 3145,
+            'level_kesulitan' => 'medium',
+            'gambar' => 'merbabu.jpg',
+            'deskripsi' => 'Gunung Merbabu adalah gunung berapi yang terletak di Jawa Tengah. Menawarkan pendakian yang menantang dengan pemandangan yang luar biasa dari puncaknya.',
+            'harga_tiket' => 250000,
+            'kuota_harian' => 60,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Lawu',
+            'lokasi_provinsi' => 'Jawa Timur',
+            'lokasi_kabupaten' => 'Kabupaten Karanganyar',
+            'ketinggian' => 3265,
+            'level_kesulitan' => 'medium',
+            'gambar' => 'lawu.jpg',
+            'deskripsi' => 'Gunung Lawu adalah gunung berapi yang terletak di perbatasan Jawa Tengah dan Jawa Timur. Dikenal dengan legenda dan pemandangan yang indah.',
+            'harga_tiket' => 200000,
+            'kuota_harian' => 70,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Arjuno-Welirang',
+            'lokasi_provinsi' => 'Jawa Timur',
+            'lokasi_kabupaten' => 'Kabupaten Malang',
+            'ketinggian' => 3339,
+            'level_kesulitan' => 'hard',
+            'gambar' => 'arjuno.jpg',
+            'deskripsi' => 'Gunung Arjuno-Welirang adalah kompleks gunung berapi di Jawa Timur. Menawarkan pendakian yang menantang dengan hutan hujan tropis yang lebat.',
+            'harga_tiket' => 400000,
+            'kuota_harian' => 40,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Kelud',
+            'lokasi_provinsi' => 'Jawa Timur',
+            'lokasi_kabupaten' => 'Kabupaten Kediri',
+            'ketinggian' => 1731,
+            'level_kesulitan' => 'easy',
+            'gambar' => 'kelud.jpg',
+            'deskripsi' => 'Gunung Kelud adalah gunung berapi aktif yang terletak di Jawa Timur. Dikenal dengan letusan dahsyatnya dan danau kawah yang indah.',
+            'harga_tiket' => 150000,
+            'kuota_harian' => 90,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Ijen',
+            'lokasi_provinsi' => 'Jawa Timur',
+            'lokasi_kabupaten' => 'Kabupaten Banyuwangi',
+            'ketinggian' => 2386,
+            'level_kesulitan' => 'medium',
+            'gambar' => 'ijen.jpg',
+            'deskripsi' => 'Gunung Ijen terkenal dengan blue fire fenomena dan danau asamnya. Terletak di perbatasan Jawa Timur dan Bali.',
+            'harga_tiket' => 350000,
+            'kuota_harian' => 50,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Prau',
+            'lokasi_provinsi' => 'Jawa Barat',
+            'lokasi_kabupaten' => 'Kabupaten Bandung Barat',
+            'ketinggian' => 2565,
+            'level_kesulitan' => 'medium',
+            'gambar' => 'prau.jpg',
+            'deskripsi' => 'Gunung Prau adalah gunung yang terletak di Jawa Barat. Dikenal dengan pemandangan sunrise yang spektakuler dan trek yang menantang.',
+            'harga_tiket' => 200000,
+            'kuota_harian' => 75,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Gede',
+            'lokasi_provinsi' => 'Jawa Barat',
+            'lokasi_kabupaten' => 'Kabupaten Cianjur',
+            'ketinggian' => 2958,
+            'level_kesulitan' => 'hard',
+            'gambar' => 'gede.jpg',
+            'deskripsi' => 'Gunung Gede adalah gunung berapi yang terletak di Jawa Barat. Bagian dari Taman Nasional Gunung Gede Pangrango dengan keanekaragaman hayati yang tinggi.',
+            'harga_tiket' => 300000,
+            'kuota_harian' => 45,
+            'status' => 'active'
+        ]);
+
+        Gunung::create([
+            'nama_gunung' => 'Gunung Ciremai',
+            'lokasi_provinsi' => 'Jawa Barat',
+            'lokasi_kabupaten' => 'Kabupaten Kuningan',
+            'ketinggian' => 3078,
+            'level_kesulitan' => 'hard',
+            'gambar' => 'ciremai.jpg',
+            'deskripsi' => 'Gunung Ciremai adalah gunung tertinggi di Jawa Barat. Dikenal dengan keindahan alamnya dan sebagai tempat ziarah spiritual.',
+            'harga_tiket' => 250000,
+            'kuota_harian' => 55,
+            'status' => 'active'
+        ]);
     }
 }
